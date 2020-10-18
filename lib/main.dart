c36ba9e1b8fa8fefcb8e31b2998b44a10ffe40b2
katalepsy <janbres666@gmail.com>
2020-10-18 23:24:36 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home:Scaffold(
+          appBar: AppBar(
+            title: Text('Im am rich'),
+            backgroundColor: Colors.amber,
+          ),
+          body: Center(child: Image(
+            image: NetworkImage('https://boocomputerservices.files.wordpress.com/2018/08/burning-pc-e1535504723170.jpg?w=321'),
+          ),
+          ),
+        )
+  )
+  );
+}
